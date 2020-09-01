8bdfe7f2626fcca238ebb30bef5bbac02c76a5f5
Mateo Gonzalez <mateo.gonzalez@fing.edu.uy>
2020-09-01 11:54:16 -0300

    Prueba AltaPlataforma

--- Workspace/src/CentralServer/AltaPlataforma.java
+++ Workspace/src/CentralServer/AltaPlataforma.java
@@ -3,8 +3,16 @@ package CentralServer;
 import java.awt.EventQueue;
 
 import javax.swing.JInternalFrame;
+import javax.swing.SpringLayout;
+import javax.swing.JScrollPane;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class AltaPlataforma extends JInternalFrame {
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -26,8 +34,44 @@ public class AltaPlataforma extends JInternalFrame {
 	 * Create the frame.
 	 */
 	public AltaPlataforma() {
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 525, 550);
+		SpringLayout springLayout = new SpringLayout();
+		getContentPane().setLayout(springLayout);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		springLayout.putConstraint(SpringLayout.NORTH, scrollPane, 10, SpringLayout.NORTH, getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, scrollPane, 10, SpringLayout.WEST, getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, scrollPane, 510, SpringLayout.NORTH, getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, scrollPane, 499, SpringLayout.WEST, getContentPane());
+		getContentPane().add(scrollPane);
+		
+		JPanel panel = new JPanel();
+		scrollPane.setViewportView(panel);
+		SpringLayout sl_panel = new SpringLayout();
+		panel.setLayout(sl_panel);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		sl_panel.putConstraint(SpringLayout.NORTH, lblNewLabel, 10, SpringLayout.NORTH, panel);
+		sl_panel.putConstraint(SpringLayout.WEST, lblNewLabel, 10, SpringLayout.WEST, panel);
+		panel.add(lblNewLabel);
+		
+		textField = new JTextField();
+		sl_panel.putConstraint(SpringLayout.NORTH, textField, -3, SpringLayout.NORTH, lblNewLabel);
+		sl_panel.putConstraint(SpringLayout.WEST, textField, 29, SpringLayout.EAST, lblNewLabel);
+		sl_panel.putConstraint(SpringLayout.EAST, textField, 189, SpringLayout.EAST, lblNewLabel);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		sl_panel.putConstraint(SpringLayout.NORTH, lblNewLabel_1, 33, SpringLayout.SOUTH, lblNewLabel);
+		sl_panel.putConstraint(SpringLayout.WEST, lblNewLabel_1, 0, SpringLayout.WEST, lblNewLabel);
+		panel.add(lblNewLabel_1);
+		
+		textField_1 = new JTextField();
+		sl_panel.putConstraint(SpringLayout.NORTH, textField_1, -3, SpringLayout.NORTH, lblNewLabel_1);
+		sl_panel.putConstraint(SpringLayout.WEST, textField_1, 0, SpringLayout.WEST, textField);
+		panel.add(textField_1);
+		textField_1.setColumns(10);
 
 	}
-
 }
