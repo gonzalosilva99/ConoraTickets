27bf0e33314be5988aaf3bc6c415bab3720726b9
Mateo Gonzalez <mateo.gonzalez@fing.edu.uy>
2020-09-08 22:40:52 -0300

    ControladorPlataforma

--- Workspace/Logica/Controladores/ControladorPlataforma.java
+++ Workspace/Logica/Controladores/ControladorPlataforma.java
@@ -11,30 +11,29 @@ import DataTypes.DtEspectaculo;
 import DataTypes.DtFuncionDatos;
 
 public class ControladorPlataforma implements IPlataforma{
-	Set<DtEspectaculo> listarEspectaculosDePlataforma() {
+	public Set<DtEspectaculo> listarEspectaculosDePlataforma() {
 		Set<DtEspectaculo> ret = new HashSet<DtEspectaculo>();
 		return ret;
 	};
-	Set<DtFuncion> listarFuncionesDeEspectaculo(String NombreEsp) {
+	public Set<DtFuncion> listarFuncionesDeEspectaculo(String NombreEsp) {
 		Set<DtFuncion> ret = new HashSet<DtFuncion>();
 		return ret;
 	};
-	DtFuncionDatos MostrarFuncion(String NombreEsp, String NombreFun) {
-		DtFuncionDatos ret = new DtFuncionDatos();
+	public DtFuncionDatos MostrarFuncion(String NombreEsp, String NombreFun) {
+		DtFuncionDatos ret = new DtFuncionDatos(NombreFun, null, null, null, null);
 		return ret;
 	};
-	DtEspectaculoDatos listarDtEspectaculoDatos(String NombreEspectaculo) {
-		DtEspectaculoDatos ret = new DtEspectaculoDatos();
+	public DtEspectaculoDatos listarDtEspectaculoDatos(String NombreEspectaculo) {
+		DtEspectaculoDatos ret = new DtEspectaculoDatos(NombreEspectaculo, NombreEspectaculo, null, null, null, NombreEspectaculo, null, null, null, null);
 		return ret;
 	};
-	Set<DtFuncion> listarFuncionesVigentesEspectaculo(String nombreEsp){
+	public Set<DtFuncion> listarFuncionesVigentesEspectaculo(String nombreEsp){
 		Set<DtFuncion> ret = new HashSet<DtFuncion>();
 		return ret;
 	};
-	void altaEspectaculo(String nickname, String nombre,String descripcion,Integer cantespmin, Integer cantmaxesp,String url,Integer costo,Date fecha) {};
-	Funcion getFuncion(String nombreEspectaculo, String NomFuncion) {
-		Date fecha1 = new Date();
-		Funcion ret = new Funcion("",fecha1,fecha1);
+	public void altaEspectaculo(String nickname, String nombre,String descripcion,Integer cantespmin, Integer cantmaxesp,String url,Integer costo,Date fecha) {};
+	public Funcion getFuncion(String nombreEspectaculo, String NomFuncion) {
+		Funcion ret = new Funcion(NomFuncion, null, null);
 		return ret;
 		
 	};
