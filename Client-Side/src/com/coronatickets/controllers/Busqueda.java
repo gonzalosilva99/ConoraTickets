dbb65f6544386d16bd5aa433bffbabdf82ef44c6
Mateo Gonzalez <mateo.gonzalez@fing.edu.uy>
2020-10-27 19:12:01 -0300

    para el gonga

---
+++ Client-Side/src/com/coronatickets/controllers/Busqueda.java
@@ -0,0 +1,63 @@
+package com.coronatickets.controllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import Controladores.Fabrica;
+import DataTypes.EstadoSesion;
+/**
+ * Servlet implementation class Home
+ */
+public class Busqueda extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Busqueda() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+	
+	/**
+	 * Devuelve el estado de la sesión
+	 * @param request
+	 */
+	private void processRequest(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String buscando = request.getParameter("bus");
+		if(buscando !=  null) {
+			PrintWriter writer = response.getWriter();
+	        writer.println("Pedido: " + buscando);
+		}
+		else {
+			PrintWriter writer = response.getWriter();
+	        writer.println("Pedido: NADA");
+		}
+	}
+	
+	
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+}
